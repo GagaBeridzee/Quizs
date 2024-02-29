0673dae74251d6faec13f30c59011194cd01d865
Gaga Beridze <GagaBeridzee@gmail.com>
2024-02-29 11:17:16 -0800

    d

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "2 + 1 = ",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "არ ვიცი არ მქონია შეხება"
+    ]
+  },
+    {
+    numb: 2,
+    question: "4 + 4 = ",
+    answer: "8",
+    options: [
+      "23",
+      "5",
+      "8",
+      "კაი ჩემო ძმაო"
+    ]
+  },
+    {
+    numb: 3,
+    question: "როგორი იქნება პრეზენტაცია?",
+    answer: "იდეალური",
+    options: [
+      "ისე რა",
+      "კარგი სავარაუდოდ",
+      "აზრზე არ ვარ ჯერ",
+      "იდეალური"
+    ]
+  },
+    {
+    numb: 4,
+    question: "რა არის კინომუსიკა?",
+    answer: "ჩვენი პროექტის სახელი",
+    options: [
+      "ჩვენი პროექტის სახელი",
+      "კინომუსიკა არის...",
+      "არ ვიცი ზუსტი პასუხი",
+      "რამე მაინც იქნება"
+    ]
+  },
+    {
+    numb: 5,
+    question: "გვეშველება?",
+    answer: "არა",
+    options: [
+      "არა",
+      "კი",
+      "კაიიიიი",
+      "ვსო ეს იყო სულ"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
